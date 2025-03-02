512da3eaff102b4f276646f3269dcc11664f7db7
lucas mendes <luckajw@gmail.com>
2025-03-02 14:54:17 -0300

    alterado estado inicial e mensagem dentro de h1.title_1

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,19 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [message, setMessage] = useState('Please select a good from the list');
+  const [message, setMessage] = useState('Please select a good from the list.');
 
   return (
     <main className="section container">
-      <h1 className="title is-flex is-align-items-center">Jam is selected</h1>
+      <h1 className="title is-flex is-align-items-center">{message}</h1>
       <table className="table">
         <tbody>
           {goods.map(good => (
-            <tr key={good} data-cy="Good" className='has-background-success-light'>
+            <tr
+              key={good}
+              data-cy="Good"
+              className="has-background-success-light"
+            >
               <td>
                 <button
                   data-cy="AddButton"
